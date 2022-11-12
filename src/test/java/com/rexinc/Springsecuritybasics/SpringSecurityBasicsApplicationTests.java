d30e0060f6fd82659db1dccc914e27ae96cfe41c
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-11-12 09:20:31 +0100

    initial commit

---
+++ src/test/java/com/rexinc/Springsecuritybasics/SpringSecurityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rexinc.Springsecuritybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
